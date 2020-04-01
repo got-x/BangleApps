d30258ddab2d5016e2437147b668b30fffa7ba5f
Tim Pahlke <t-pahlke@web.de>
2020-04-01 20:02:23 +0200

    Updated gbaudioc.js

--- apps/gbaudioc/gbaudioc.js
+++ apps/gbaudioc/gbaudioc.js
@@ -2,26 +2,29 @@ function init() {
     setWatch(btn1, BTN1);
     setWatch(btn2, BTN2);
     setWatch(btn3, BTN3);
-    Bangle.on('touch', function(b) { 
-        b == 1 ? t1(): t2();
+    Bangle.on('touch', function(b) {
+      if (b == 1)
+        t1();
+      else
+        t2();
     });
     display();
 }
 
 function btn1() {
-    Bluetooth.println(JSON.stringify({t:"info", msg:"btn1"}))
+    Bluetooth.println(JSON.stringify({t:"info", msg:"btn1"}));
 }
 function btn2() {
-    Bluetooth.println(JSON.stringify({t:"info", msg:"btn2"}))
+    Bluetooth.println(JSON.stringify({t:"info", msg:"btn2"}));
 }
 function btn3() {
-    Bluetooth.println(JSON.stringify({t:"info", msg:"btn3"}))
+    Bluetooth.println(JSON.stringify({t:"info", msg:"btn3"}));
 }
 function t1() {
-    Bluetooth.println(JSON.stringify({t:"info", msg:"t1"}))
+    Bluetooth.println(JSON.stringify({t:"info", msg:"t1"}));
 }
 function t2() {
-    Bluetooth.println(JSON.stringify({t:"info", msg:"t2"}))
+    Bluetooth.println(JSON.stringify({t:"info", msg:"t2"}));
 }
 
 function display() {
